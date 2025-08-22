eb3f81bcffb29305be8a248b24338b4551b8b508
hayato81 <hayatomanex0922@gmail.com>
2025-08-22 18:44:38 +0900

    spring2のみに変更

---
+++ springboot_todoApp-2/src/main/java/com/example/demo/SpringbootTodoApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodoApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTodoApp2Application.class, args);
+	}
+
+}
