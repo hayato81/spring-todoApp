674d7c3e5ac92600312d0f0623b6b3f93a0de4f7
wataru <wataru.corder@gmail.com>
2025-08-23 21:43:19 +0900

    MyBatisのTaskMapperを追加し、tasksからの一覧取得(findAll)を実装

--- springboot_todoApp-2/src/main/java/com/example/demo/mapper/TaskMapper.java
+++ springboot_todoApp-2/src/main/java/com/example/demo/mapper/TaskMapper.java
@@ -1,5 +1,25 @@
 package com.example.demo.mapper;
 
-public class TaskMapper {
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import sample.common.dao.entity.Task;
+
+
+@Mapper
+public interface TaskMapper {
+
+	@Select("""
+			  SELECT
+			    id, username, title, content, name,
+			    start_date AS startDate,
+			    end_date   AS endDate,
+			    created_at AS createdAt,
+			    updated_at AS updatedAt
+			  FROM tasks
+			""")
+	public List<Task> findAll();
 
 }
