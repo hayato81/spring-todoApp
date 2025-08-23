be7c9e4205bbd1718353f57d7d1f0297ba203171
wataru <wataru.corder@gmail.com>
2025-08-24 02:51:26 +0900

    Lombokの実装

--- springboot_todoApp-2/src/main/java/sample/common/dao/entity/Task.java
+++ springboot_todoApp-2/src/main/java/sample/common/dao/entity/Task.java
@@ -3,6 +3,9 @@ package sample.common.dao.entity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
 public class Task {
 
     private Long id;                  // BIGINT
@@ -15,31 +18,4 @@ public class Task {
     private LocalDateTime createdAt;  // TIMESTAMP
     private LocalDateTime updatedAt;  // TIMESTAMP
 
-    // --- Getter & Setter ---
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public LocalDate getStartDate() { return startDate; }
-    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
-
-    public LocalDate getEndDate() { return endDate; }
-    public void setEndDate(LocalDate endDate) { this.endDate = endDate; }
-
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
-
-    public LocalDateTime getUpdatedAt() { return updatedAt; }
-    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
 }
