373410e28acd584275f2b8aea9b282a560085f03
wataru <wataru.corder@gmail.com>
2025-08-23 23:06:49 +0900

    EntityからDTOに変換を実装

--- springboot_todoApp-2/src/main/java/com/example/demo/thymeleaf/web/TaskController.java
+++ springboot_todoApp-2/src/main/java/com/example/demo/thymeleaf/web/TaskController.java
@@ -1,13 +1,17 @@
 package com.example.demo.thymeleaf.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.service.TaskService;
+import com.example.demo.thymeleaf.web.tasks.TaskListDTO;
 
 import lombok.RequiredArgsConstructor;
+import sample.common.dao.entity.Task;
 
 @Controller
 @RequestMapping("/tasks")
@@ -18,9 +22,15 @@ public class TaskController {
 
 	@GetMapping
 	public String list(Model model) {
-		var taskList =  taskService.find();
+		List<Task> taskEntity = taskService.find();
+		var taskList = taskEntity
+					   .stream()
+					   .map((entity) -> TaskListDTO.from(entity))
+					   .toList();
 		model.addAttribute("taskList",taskList);
 		return "tasks/list";
 	}
+	
+	
 
 }
