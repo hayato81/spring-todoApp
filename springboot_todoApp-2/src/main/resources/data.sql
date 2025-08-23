908eb37702ca49c570cb6e60348223acfe46596a
wataru <wataru.corder@gmail.com>
2025-08-23 21:12:28 +0900

    テストデータベースにデータ挿入

---
+++ springboot_todoApp-2/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tasks (
+  username, title, content, name, start_date, end_date
+) VALUES (
+  'john', 'First Task', 'Sample content', 'John Doe', DATE '2025-08-23', DATE '2025-09-01'
+);
